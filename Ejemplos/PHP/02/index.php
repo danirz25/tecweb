2668eb117b0f6e8a595621eeee31f8784f409593
Dani <daniel.patino@alumno.buap.mx>
2024-10-29 11:43:55 -0600

    Actividad de la práctica 12

---
+++ Ejemplos/PHP/02/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo 2</title>
+</head>
+<body>
+    <?php
+     require_once __DIR__ . '/Menu.php';
+    $menu = new Menu();
+    $menu->cargar_opcion('https://www.facebook.com', 'Facebook');
+    $menu->cargar_opcion('https://www.instagram.com', 'Instagram');
+    $menu->cargar_opcion('https://www.twitter.com', 'X');
+    $menu->mostrar();
+    ?>
+</body>
+</html>
---
+++ Ejemplos/PHP/03/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo 3</title>
+</head>
+<body>
+    <?php
+    use EJEMPLOS\POO\Cabecera2 as Cabecera;
+    require_once __DIR__ . '/Cabecera.php';
+
+    $cab = new Cabecera('El rincón del programador ','center', 'https://cs.buap.mx/');
+    $cab->graficar();
+    ?>
+</body>
+</html>
